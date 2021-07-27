d85242f86206ec5c3453b55c0e891a7cc094ed81
Sameque Sant' ana <samequesantana@gmail.com>
2021-07-27 16:41:12 -0300

    repository origin

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -11,21 +11,25 @@ namespace Api.Controllers
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
+
         }
 
         [HttpPost]
         public void Post([FromBody] string value)
         {
+
         }
 
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+
         }
     }
 }
